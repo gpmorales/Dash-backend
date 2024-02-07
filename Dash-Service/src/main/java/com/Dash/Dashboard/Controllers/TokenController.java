1cca5db5d06bb723c585e09cf1e627fd49fa4e77
gpmorales <georgepm20002@gmail.com>
2024-02-07 16:20:10 -0500

    modified file

---
+++ Dash-Service/src/main/java/com/Dash/Dashboard/Controllers/TokenController.java
@@ -0,0 +1,11 @@
+package com.Dash.Dashboard.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+    public void foo() {
+
+    }
+}
